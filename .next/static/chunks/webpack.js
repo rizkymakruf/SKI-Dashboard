3d3511cf4b01db303d5831b60a41360129a5ebf6
rizkymakruf <armppy8@gmail.com>
2022-06-10 16:30:41 +0800

    initial commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "99e59eaff9603c44"; }
+/******/ 		__webpack_require__.h = function() { return "105b4c4c0dc1b45f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
