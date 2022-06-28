275c306669c9a5fc95675572654f866bbca41251
rizky makruf <72442752+rizkymakruf@users.noreply.github.com>
2022-06-28 13:15:53 +0800

    initial commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "44de05d8cebcd34a"; }
+/******/ 		__webpack_require__.h = function() { return "f73d39b1abf946c4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
