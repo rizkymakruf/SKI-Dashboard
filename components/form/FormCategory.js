0e5ea115efe96027b47fdedc7964e8f302c43854
rizky makruf <72442752+rizkymakruf@users.noreply.github.com>
2022-07-06 17:06:42 +0800

    initial commit

--- components/form/FormCategory.js
+++ components/form/FormCategory.js
@@ -1,32 +1,26 @@
-import React, { useCallback, useEffect } from "react";
-import { useForm } from "react-hook-form";
+// import { useForm } from "react-hook-form";
+// import { categorySchema } from "./Validation/category";
+import { useFormik } from "formik";
 
-export default function FormCategory() {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-    clearErrors,
-    trigger,
-  } = useForm();
+function FormCategory() {
+  // const onSubmit = async (event) => {
+  //   event.preventDefault();
+  //   let formData = {
+  //     name: event.target[0].value,
+  //   };
+  //   const isValid = await categorySchema.isValid(formData);
+  //   console.log("valid?", isValid);
+  // };
 
-  const onSubmit = async (e) => {
-    // api goes here
-
-    reset();
-    clearErrors("category");
-  };
-
-  useEffect(() => {
-    // validate onMount
-    // handleSubmit(onSubmit)();
-    // eslint-disable-next-line
-  }, []);
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+    },
+  });
 
   return (
     <div className="w-full h-auto">
-      <form onSubmit={() => onSubmit()}>
+      <form onSubmit={onSubmit}>
         <div className="w-full h-full select-none">
           <div className="w-full h-full p-3 backdrop-blur bg-white/30 rounded-md border border-gray-300 shadow-md">
             <div className="w-full h-auto relative mb-4">
@@ -35,30 +29,22 @@ export default function FormCategory() {
               </p>
               <input
                 type="text"
-                name="category"
+                name="name"
+                autoComplete="off"
                 placeholder="Category Name"
-                className="placeholder-gray-400 h-10 bg-gray-500/20 form-input mt-1 rounded-md border border-gray-300 w-full focus:ring-2 duration-500 focus:ring-blue-500"
-                {...register("category", { minLength: 3 })}
-                // onKeyUp={() => {
-                //   trigger("category");
-                // }}
+                className={`w-full shadow py-3 px-4 placeholder-gray-500 border-gray-300 rounded-md focus:outline-none focus:ring-2 `}
+                // {...register("name", { required: "password is required!" })}
               />
-
-              {errors.category && (
-                <p className="text-xs text-red-500 pt-2">
-                  Minimum length 3 character !
-                </p>
-              )}
+              {/* ${errors.name ? "ring-2 ring-red-500" : null} */}
+              {/* <p className="text-xs text-red-500 pt-2">
+                {errors.name?.message}
+              </p> */}
             </div>
 
             <div className="w-full h-12 flex justify-between gap-2">
-              <div className="w-full h-full flex items-center text-xs text-red-400">
-                {/* {globalCtx.errorMsg} */}
-              </div>
               <div className="w-auto h-full flex items-center gap-x-2">
                 <button
-                  // type="submit"
-                  // disabled={globalCtx.isFetch ? "disabled" : ""}
+                  type="submit"
                   className="px-6 h-8 bg-green-500/30 text-green-500 border-2 shadow-md hover:bg-green-500/50 border-green-300 font-semibold rounded overflow-hidden"
                 >
                   Create
@@ -71,3 +57,5 @@ export default function FormCategory() {
     </div>
   );
 }
+
+export default FormCategory;
